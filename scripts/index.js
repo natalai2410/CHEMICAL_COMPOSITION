7230e0c887e462bc4d9c0779c21ac437c3766689
natalai2410 <58889273+natalai2410@users.noreply.github.com>
2022-06-08 01:23:06 +0300

    правки

--- scripts/index.js
+++ scripts/index.js
@@ -6,34 +6,17 @@ const template = document.querySelector('.template');
 const listContainer = document.querySelector('.table__content');
 
 function addNewLine() {
-
     listContainer.append(createNewRecord("name", "value") );
-}
-
-function removeLine(event) {
- const element = event.target.closest('.table__item');
-    element.remove();
-    checkEmpty();
-}
-
-function loadComponent() {
-    //представили как массив все кнопки
-    const elements  = Array.from(document.querySelectorAll('.table__cross-btn'));
-    elements.forEach(function (item) {
-        console.log(item);
-       item.addEventListener('click', removeLine);
-    });
 }
 
 function checkEmpty()
 {
     const elements  = Array.from(document.querySelectorAll('.table__item'));
-    if (elements.length < 2) {
+    if (elements.length > 45) {
         acceptBtn.classList.toggle('chemical-composition__primary-default-btn_disabled');
         acceptBtn.disabled = true;
     }
 }
-
 
 addLineBtn.addEventListener('click', addNewLine);
 
@@ -44,14 +27,10 @@ function createNewRecord(name, value) {
     const newName = newLine.getElementById('line-name');
     const newValue = newLine.getElementById('line-value');
 
-    //const newRemoveBtn = newLine.querySelector('.table__cross-btn');
-    //newRemoveBtn.addEventListener('click', removeLine);
-
     newName.textContent = name;
     newValue.textContent = value;
+
     checkEmpty();
 
     return newLine;
 }
-
-loadComponent();
